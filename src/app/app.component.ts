e160d6134bd9dac0e3de0d29c268eeb0bd0fa00c
Jessica <jessica@email.com>
2020-04-09 11:13:11 +0000

    Inbound properties  and feature module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alurapic';
-  src = 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Little-Fairy-Girl.jpg';
-  alt = 'fada';
+
 }
