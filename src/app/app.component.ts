72c42d139c07fcbbc6a41a5367b0c87ec48cf2f8
Jessica <jessica@email.com>
2020-04-09 08:44:34 +0000

    Bootstrap downloaded via npm and added on global scope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alurapic';
+  src = 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Little-Fairy-Girl.jpg';
+  alt = 'fada';
 }
