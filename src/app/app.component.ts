79467af0e136f72df0ce0ce7aae5fca05556bfc9
Jessica <jessica@email.com>
2020-04-12 14:26:45 +0000

    Dependency injection, providers, photo interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from 'src/photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  photos: Object[] = [];
+
+  constructor(photoService : PhotoService){
+
+    photoService
+    .listFromUser('flavio')
+    .subscribe(photos => this.photos = photos);
+  }
 }
