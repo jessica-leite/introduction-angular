07575dd2ab0859be8d79250065304da61e6d2158
Jessica <jessica@email.com>
2020-04-17 17:07:54 +0000

    Output property (custom event), search component

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -10,11 +8,10 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit{
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -28,20 +25,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos; //or this.activatedRoute.snapshot.data['photos'];
     this.userName = this.activatedRoute.snapshot.params.userName;
-
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load(){
     this.photoService
       .listFromUserPaginated(this.userName ,++this.currentPage)
-      .subscribe(photos => {this.photos = this.photos.concat(photos); if(!photos.length) this.hasMore = false;})
+      .subscribe(photos => {
+        this.filter = ''; 
+        this.photos = this.photos.concat(photos); 
+        if(!photos.length) this.hasMore = false;
+      });
   }
 
 }
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,13 +6,15 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
