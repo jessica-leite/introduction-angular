17c07ad297823bfb24f1a1f604b231b4891af8a2
Jessica <jessica@email.com>
2020-05-13 14:07:29 +0000

    Login component created

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+
+@NgModule({
+    declarations: [ SignInComponent ]
+})
+export class HomeModule { }
