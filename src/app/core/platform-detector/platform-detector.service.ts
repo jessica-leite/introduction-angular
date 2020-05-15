d2672240ec3215eea1a5711b3c80664f13aab5c5
Jessica <jessica@email.com>
2020-05-15 17:47:37 +0000

    Platform detector service

---
+++ src/app/core/platform-detector/platform-detector.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({ providedIn: 'root' })
+export class PlatformDetectorService {
+    constructor(@Inject(PLATFORM_ID) private platformId: string) { }
+
+    isPlatformBrowser() {
+        return isPlatformBrowser(this.platformId);
+    }
+}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/core/auth.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
 import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -9,12 +10,13 @@ import { Router } from '@angular/router';
 export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
-    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>; 
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ) { }
 
     ngOnInit(): void {
@@ -29,13 +31,13 @@ export class SignInComponent implements OnInit {
         const password = this.loginForm.get('password').value;
 
         this.authService.authenticate(userName, password)
-        .subscribe(
-            () => this.router.navigate(['user', userName]),
-            err => {
-                console.log(err);
-                this.loginForm.reset();
-                this.userNameInput.nativeElement.focus();
-            }
-        );
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    this.platformDetectorService.isPlatformBrowser() && this.userNameInput.nativeElement.focus();
+                }
+            );
     }
 }
