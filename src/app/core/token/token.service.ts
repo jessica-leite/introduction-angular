c2400075f88a6c027f8efec50edecc56de7b59b5
Jessica <jessica@email.com>
2020-05-16 14:37:08 +0000

    Observable headers and localStorage

---
+++ src/app/core/token/token.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'authToken';
+
+@Injectable({ providedIn: 'root' })
+export class TokenService {
+
+    hasToken() {
+        return !!this.getToken();
+    }
+
+    getToken() {
+        return window.localStorage.getItem(KEY);
+    }
+
+    setToken(token: string) {
+        window.localStorage.setItem(KEY, token);
+    }
+
+    removeToken() {
+        window.localStorage.removeItem(KEY);
+    }
+}
