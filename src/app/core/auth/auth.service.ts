6fa86774d56b10d5cb34807bbb2a99e09843c5b5
Jessica <jessica@email.com>
2020-05-18 16:19:08 +0000

    jwt-decode implemented and User service created

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,7 +12,7 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService
+    private userService: UserService
   ) { }
 
   authenticate(userName: string, password: string) {
@@ -20,7 +20,7 @@ export class AuthService {
     return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
       }));
   }
 }
