227bf5062a65ebc1d1cd2d44d33389826a8bb3eb
Jessica <jessica@email.com>
2020-05-20 12:03:03 +0000

    Route Guards

---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { UserService } from '../user/user.service';
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+
+    constructor(private userService: UserService, private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+
+        if (this.userService.isLogged()) {
+
+            this.router.navigate(['user', this.userService.getUserName()])
+            return false;
+        }
+
+        return true;
+    }
+}
