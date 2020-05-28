aef6628a00f400b869e70b9ae559a2de61d1a248
Jessica <jessica@email.com>
2020-05-28 11:43:42 +0000

    Asynchronous validator for username

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,46 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
-    templateUrl: './signup.component.html'
+  templateUrl: './signup.component.html'
 })
 export class SignUpComponent implements OnInit {
 
-    private signupForm: FormGroup
+  private signupForm: FormGroup
 
-    constructor(private formBuider: FormBuilder) { }
+  constructor(
+    private formBuider: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService
+  ) { }
 
-    ngOnInit(): void {
-        this.signupForm = this.formBuider.group({
-            email: ['',
-                [
-                    Validators.required,
-                    Validators.email
-                ]
-            ],
-            fullName: ['', 
-                [
-                    Validators.required,
-                    Validators.minLength(2),
-                    Validators.maxLength(40)
-                ]
-            ],
-            userName: ['', 
-                [
-                    Validators.required,
-                    lowerCaseValidator,
-                    Validators.minLength(2),
-                    Validators.maxLength(30)
-                ]
-            ],
-            password: ['', 
-                [
-                    Validators.required,
-                    Validators.minLength(8),
-                    Validators.maxLength(14)
-                ]
-            ]   
-        })
-    }
+  ngOnInit(): void {
+    this.signupForm = this.formBuider.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          lowerCaseValidator,
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+    })
+  }
 }
---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { SignUpService } from './signup.service';
+import { AbstractControl } from '@angular/forms';
+import { debounceTime, switchMap, map, first } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
+  constructor(private signUpService: SignUpService) { }
+
+  checkUserNameTaken() {
+    return (control: AbstractControl) => {
+      return control.valueChanges
+        .pipe(debounceTime(300))
+        .pipe(
+          switchMap((userName) =>
+            this.signUpService.checkUserNameTaken(userName)
+          )
+        )
+        .pipe(map((isTaken) => (isTaken ? { userNameTaken: true } : null)))
+        .pipe(first());
+    };
+  }
+}
