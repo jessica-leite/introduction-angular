1cd166b7a87d1d7cfc7d14fa7acf62fa1f039a6d
Jessica <jessica@email.com>
2020-05-28 13:31:38 +0000

    Creating user via post

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,51 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
 
 @Component({
-  templateUrl: './signup.component.html'
+    templateUrl: './signup.component.html'
 })
 export class SignUpComponent implements OnInit {
 
-  private signupForm: FormGroup
+    private signupForm: FormGroup
 
-  constructor(
-    private formBuider: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService
-  ) { }
+    constructor(
+        private formBuider: FormBuilder,
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-    this.signupForm = this.formBuider.group({
-      email: ['',
-        [
-          Validators.required,
-          Validators.email
-        ]
-      ],
-      fullName: ['',
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(40)
-        ]
-      ],
-      userName: ['',
-        [
-          Validators.required,
-          lowerCaseValidator,
-          Validators.minLength(2),
-          Validators.maxLength(30)
-        ],
-        this.userNotTakenValidatorService.checkUserNameTaken()
-      ],
-      password: ['',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.maxLength(14)
-        ]
-      ]
-    })
-  }
+    ngOnInit(): void {
+        this.signupForm = this.formBuider.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['',
+                [
+                    Validators.required,
+                    lowerCaseValidator,
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ],
+                this.userNotTakenValidatorService.checkUserNameTaken()
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ]
+            ]
+        })
+    }
+
+    signup() {
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService.signup(newUser)
+            .subscribe(
+                () => this.router.navigate(['']),
+                err => console.log(err)
+            );
+    }
 }
